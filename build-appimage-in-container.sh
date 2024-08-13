7db5c15ad9755124b6367f41bd3fd1fd61a395d7
Pedro Terzero <pedro@terzero>
2024-08-13 23:57:22 +0200

    always make OpenXcomEx executable

--- build-appimage-in-container.sh
+++ build-appimage-in-container.sh
@@ -6,7 +6,8 @@ cp -a vol/oxce/. AppDir/.
 rm AppDir/OpenXcomEx
 
 mkdir -p AppDir/usr/bin \
-  && cp vol/oxce/OpenXcomEx AppDir/usr/bin/
+  && cp vol/oxce/OpenXcomEx AppDir/usr/bin/ \
+  && chmod +x AppDir/usr/bin/OpenXcomEx
 
 /usr/bin/linuxdeploy --appdir AppDir --output appimage
 mv *.AppImage out/
